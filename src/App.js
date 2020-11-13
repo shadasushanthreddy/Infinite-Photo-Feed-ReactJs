4ec4b941674adf8e375d43eea7ff719c79d9e463
shadasushanthreddy <42132187+shadasushanthreddy@users.noreply.github.com>
2020-11-14 01:38:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { UnsplashImage } from './components/UnsplashImage';
+import { Loader } from './components/Loader';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Masonry from 'react-masonry-css';
+import styled from 'styled-components';
+import Image from './Image';
+import { createGlobalStyle } from 'styled-components';
+
+// Style
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+  }
+`;
+
+const WrapperImages = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-auto-rows: 300px;
+`;
+
+function App() {
+  const [images, setImage] = useState([]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [])
+
+  const fetchImages = (count = 20) => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey = "a45d8d5153b51bd885114b73a6719941a9b9f3c369b63d3aafb3bba59364abd5";
+
+    axios
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+      .then(res => {
+        setImage([...images, ...res.data]);
+      })
+  }
+
+  
+  return (
+    
+    <div>
+      <h1>"YELLOW CLASS REACT ASSESSMENT"</h1>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={fetchImages}
+        hasMore={true}
+        loader={<Loader />}
+      >
+        <WrapperImages>
+          {images.map(image => (
+            <UnsplashImage url={image.urls.thumb} key={image.id} />
+          ))}
+        </WrapperImages>
+      </InfiniteScroll>
+    </div>
+  );
+}
+
+export default App;
